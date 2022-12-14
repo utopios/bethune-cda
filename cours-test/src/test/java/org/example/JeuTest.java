c475d88ee68f5bbb7f8a55d97278a0650db99483
ihab abadi <ihab@utopios.net>
2022-12-14 15:02:23 +0100

    avec mockito

--- cours-test/src/test/java/org/example/JeuTest.java
+++ cours-test/src/test/java/org/example/JeuTest.java
@@ -1,16 +1,35 @@
 package org.example;
 
+import org.example.interfaces.IDe;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class JeuTest {
 
     private Jeu jeu;
 
+    @Mock
+    private De de;
     @BeforeEach
     void setUp() {
-        jeu = new Jeu(new FauxDe());
+        //Methode 1
+        //jeu = new Jeu(new FauxDe());
+        //Methode 2
+        /*jeu = new Jeu(new IDe() {
+            @Override
+            public int getValue() {
+                return 6;
+            }
+        });*/
+        //Methode 3
+        jeu  = new Jeu(de);
+        Mockito.when(de.getValue()).thenReturn(6);
     }
 
     @Test
@@ -20,7 +39,13 @@ public class JeuTest {
 
     @Test
     void testJouerNotWin() {
-        jeu = new Jeu(new FauxDeNotWin());
+        /*jeu = new Jeu(new IDe() {
+            @Override
+            public int getValue() {
+                return 0;
+            }
+        });*/
+        Mockito.when(de.getValue()).thenReturn(3);
         Assertions.assertFalse(jeu.jouer());
     }
 }
