43bf5bf3b181758a4694704696321258468c633f
ihab abadi <ihab@utopios.net>
2022-12-19 11:40:52 +0100

    structure spring

---
+++ cours-spring/src/main/java/com/example/coursspring/CoursSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursSpringApplication.class, args);
+    }
+
+}
