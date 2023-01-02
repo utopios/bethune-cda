ec6d1c383b00b960b06be7c85b9092da57537fe3
ihab abadi <ihab@utopios.net>
2023-01-02 16:05:44 +0100

    utilisation des sessions

---
+++ cours-spring/src/main/java/com/example/coursspring/controller/SessionController.java
@@ -0,0 +1,28 @@
+package com.example.coursspring.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("http-session")
+@ResponseBody
+public class SessionController {
+
+    @Autowired
+    private HttpSession _httpSession;
+
+    @GetMapping("ecrire")
+    public String write() {
+        _httpSession.setAttribute("name", "ihab");
+        return "Ok";
+    }
+
+    @GetMapping("lire")
+    public String read() {
+        return _httpSession.getAttribute("name").toString();
+    }
+}
