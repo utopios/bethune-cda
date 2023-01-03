7614a57d0991307a24435c8d6751e6f587a67d0e
ihab abadi <ihab@utopios.net>
2023-01-03 09:22:37 +0100

    enregistrer des objets dans un cookie

--- cours-spring/src/main/java/com/example/coursspring/controller/CookieController.java
+++ cours-spring/src/main/java/com/example/coursspring/controller/CookieController.java
@@ -1,5 +1,7 @@
 package com.example.coursspring.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @ResponseBody
 @RequestMapping("cours-cookie")
@@ -19,6 +26,21 @@ public class CookieController {
     @GetMapping("ecrire")
     public String writeCookie(HttpServletResponse response) {
         Cookie cookie = new Cookie("name", "Ihab");
+        //La durée de vie du cookie
+        cookie.setMaxAge(365*24*3600);
+        //Si la durée de vie est à 0, le cookie sera supprimé
+        //cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "cookie writed";
+    }
+
+    @GetMapping("ecrire-list")
+    public String writeCookieList(HttpServletResponse response) throws JsonProcessingException, UnsupportedEncodingException {
+        List<String> stringList = Arrays.asList("toto","tata", "titi");
+        //Convertion d'un objet en json avec api jackson
+        ObjectMapper mapper = new ObjectMapper();
+        String stringListJson = mapper.writeValueAsString(stringList);
+        Cookie cookie = new Cookie("name", URLEncoder.encode(stringListJson, "UTF-8"));
         //La durée de vie du cookie
         cookie.setMaxAge(365*24*3600);
         //Si la durée de vie est à 0, le cookie sera supprimé
