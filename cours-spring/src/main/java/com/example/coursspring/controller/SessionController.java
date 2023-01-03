3bd29a347eafa31ac1cc1fd6358598109602df58
ihab abadi <ihab@utopios.net>
2023-01-03 09:29:45 +0100

    stockage objet dans un HttpSession

--- cours-spring/src/main/java/com/example/coursspring/controller/SessionController.java
+++ cours-spring/src/main/java/com/example/coursspring/controller/SessionController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("http-session")
 @ResponseBody
@@ -21,9 +24,21 @@ public class SessionController {
         return "Ok";
     }
 
+    @GetMapping("ecrire-list")
+    public String writeList() {
+        List<String> stringList = Arrays.asList("toto","tata", "titi");
+        _httpSession.setAttribute("liste", stringList);
+        return "Ok";
+    }
+
     @GetMapping("lire")
     public String read() {
         return _httpSession.getAttribute("name").toString();
+    }
+
+    @GetMapping("lire-list")
+    public List<String> readList() {
+        return (List<String>) _httpSession.getAttribute("liste");
     }
     @GetMapping("supprimer")
     public String remove() {
