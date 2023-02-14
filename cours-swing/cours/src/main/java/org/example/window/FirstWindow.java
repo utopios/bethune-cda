2d4be1c177ff3a3d4146c0316bbf39dcdb63cb2f
ihab abadi <ihab@utopios.net>
2023-02-14 11:29:21 +0100

    debut du cours swing

--- cours-swing/cours/src/main/java/org/example/window/FirstWindow.java
+++ cours-swing/cours/src/main/java/org/example/window/FirstWindow.java
@@ -3,32 +3,64 @@ package org.example.window;
 import lombok.Data;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 @Data
 public class FirstWindow {
-    private JButton firstButton;
+   /* private JButton firstButton;*/
     private JPanel firstPanel;
-    private JRadioButton radioButton1;
-    private JButton button2;
-    private JButton button3;
     private int count = 0;
     public FirstWindow() {
-        firstButton.setText("Je suis un bouton");
+        /*firstButton.setText("Je suis un bouton");
 
         //Event sur le click
-        /*firstButton.addActionListener(new ActionListener() {
+        *//*firstButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 count++;
                 firstButton.setText(firstButton.getText() + " click : "+count);
             }
-        });*/
+        });*//*
         firstButton.addActionListener((e) -> {
             System.out.println(e);
             count++;
             firstButton.setText(((JButton)e.getSource()).getText() + " Click : "+count);
-        });
+        });*/
+        //Utilisation des GridLayout
+        /*firstPanel.setLayout(new GridLayout(3,3));
+        for(int i=1; i <= 9; i++) {
+            JButton b = new JButton("b" + i);
+            b.addActionListener((e) -> {
+                System.out.println(((JButton)e.getSource()).getText());
+            });
+            firstPanel.add(b);
+        }*/
+        //Utilisation des GridBagLayout
+        GridBagLayout g = new GridBagLayout();
+        g.columnWidths = new int[6];
+        g.rowHeights = new int[6];
+        firstPanel.setLayout(new GridBagLayout());
+        for(int i=1; i <= 3; i++) {
+            for(int j=1; j <= 3; j++) {
+                JButton b = new JButton("b" + i+ " X "+j);
+                GridBagConstraints c = new GridBagConstraints();
+                c.fill = GridBagConstraints.HORIZONTAL;
+                c.gridy = j;
+                c.gridx = i;
+                if(i%2 == 0) {
+                    c.gridwidth = 2;
+                }
+                /*else {
+                    c.gridheight = 2;
+                }*/
+
+                b.addActionListener((e) -> {
+                    System.out.println(((JButton)e.getSource()).getText());
+                });
+                firstPanel.add(b, c);
+            }
+        }
     }
 }
