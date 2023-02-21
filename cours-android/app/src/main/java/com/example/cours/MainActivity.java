68d604591e4e021f34b1769a6b9db2885671c1cc
ihab abadi <ihab@utopios.net>
2023-02-21 10:02:41 +0100

    cours android

---
+++ cours-android/app/src/main/java/com/example/cours/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cours;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Ajouter des éléments par le code dans mon activité.
+        LinearLayout mainLayout  = findViewById(R.id.main_linear_layout);
+        for(int i = 1; i < 4; i++) {
+            Button b = new Button(this);
+            b.setText("contenu button" + i);
+            mainLayout.addView(b);
+        }
+    }
+}
