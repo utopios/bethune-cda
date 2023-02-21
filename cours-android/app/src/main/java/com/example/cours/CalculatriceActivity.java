07cefa8bf3edc757e7e6b1d30b76afb269f87b8c
ihab abadi <ihab@utopios.net>
2023-02-21 14:13:40 +0100

    correction calculatrice

---
+++ cours-android/app/src/main/java/com/example/cours/CalculatriceActivity.java
@@ -0,0 +1,61 @@
+package com.example.cours;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class CalculatriceActivity extends AppCompatActivity {
+
+    private TableLayout calculatriceLayout;
+    private String[] elements = {"C", "+/-", "%", "/", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", "0", ",","="};
+    private TableRow row;
+    private TextView screen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice);
+        calculatriceLayout = findViewById(R.id.calculatrice_table);
+        screen = findViewById(R.id.screen);
+        generateTable();
+    }
+
+    private void generateTable() {
+        int x = 0;
+        int y = 0;
+        for(String s : elements) {
+            if(x == 0 || (x) % 4 == 0) {
+                row = new TableRow(this);
+                calculatriceLayout.addView(row);
+                y++;
+            }
+            Button b = new Button(this);
+            b.setText(s);
+            TableRow.LayoutParams params =new TableRow.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            params.weight = 1;
+            if(y == 5) {
+                params.weight = 1;
+                if (s == "0") {
+                    params.weight = 10;
+                }
+            }
+
+            b.setLayoutParams(params);
+            b.setOnClickListener((e) -> {
+                actionButton((Button) e);
+            });
+            row.addView(b);
+            x++;
+        }
+    }
+
+    private void actionButton(Button b) {
+        screen.setText(screen.getText()+""+b.getText());
+    }
+}
--- cours-android/app/src/main/java/com/example/cours/MainActivity.java
+++ cours-android/app/src/main/java/com/example/cours/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.cours;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
             b.setText("contenu button" + i);
             mainLayout.addView(b);
         }*/
+
+        //Retrouver les éléments d'un layout
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        TableLayout tableLayout = findViewById(R.id.main_table_layout);
+        for(int i = 0; i < tableLayout.getChildCount(); i++) {
+            TableRow row = (TableRow) tableLayout.getChildAt(i);
+            for(int j=0; j < row.getChildCount(); j++) {
+                Button b = (Button) row.getChildAt(j);
+                //b.setText("edit button");
+                b.setOnClickListener((e) -> {
+                   dialogBuilder.setMessage(((Button)e).getText()).create().show();
+                });
+            }
+        }
     }
 }
