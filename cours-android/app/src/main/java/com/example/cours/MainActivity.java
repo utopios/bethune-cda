12691222e41aea7709dc92655d4b165755096816
ihab abadi <ihab@utopios.net>
2023-02-21 10:14:13 +0100

    cours android

--- cours-android/app/src/main/java/com/example/cours/MainActivity.java
+++ cours-android/app/src/main/java/com/example/cours/MainActivity.java
@@ -13,11 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Ajouter des éléments par le code dans mon activité.
-        LinearLayout mainLayout  = findViewById(R.id.main_linear_layout);
+        /*LinearLayout mainLayout  = findViewById(R.id.main_linear_layout);
         for(int i = 1; i < 4; i++) {
             Button b = new Button(this);
             b.setText("contenu button" + i);
             mainLayout.addView(b);
-        }
+        }*/
     }
 }
