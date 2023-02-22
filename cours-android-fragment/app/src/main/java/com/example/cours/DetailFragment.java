fed28f74b3ce83ebb1e7c20ce432e134cc3ed627
ihab abadi <ihab@utopios.net>
2023-02-22 11:15:42 +0100

    exercice de navigation avec fragment

---
+++ cours-android-fragment/app/src/main/java/com/example/cours/DetailFragment.java
@@ -0,0 +1,35 @@
+package com.example.cours;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DetailFragment extends Fragment {
+
+
+
+    public DetailFragment() {
+
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
---
+++ cours-android-fragment/app/src/main/java/com/example/cours/FormFragment.java
@@ -0,0 +1,51 @@
+package com.example.cours;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cours.databinding.FragmentFirstBinding;
+import com.example.cours.databinding.FragmentFormBinding;
+
+
+public class FormFragment extends Fragment {
+
+    public FragmentFormBinding binding;
+
+
+    //Construction du fragment
+    public FormFragment() {
+        // Required empty public constructor
+    }
+
+
+    //Après la création de l'objet
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+    //La création de la vue du fragment
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentFormBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    //Après la création de la view du fragment
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        binding.validButton.setOnClickListener((e) -> {
+            FormFragmentDirections.ActionFromToDetail action = FormFragmentDirections.actionFromToDetail(binding.firstnameEdittext.getText().toString(), binding.lastnameEdittext.getText().toString(), binding.phoneEdittext.getText().toString());
+            NavHostFragment.findNavController(FormFragment.this).navigate(action);
+        });
+    }
+}
